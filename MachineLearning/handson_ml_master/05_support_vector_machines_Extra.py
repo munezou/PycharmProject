c4644dfd696742f0ce44dd19746a89baa986949d
munezou <kazumikm0119@pi5.fiberbit.net>
2019-11-04 12:20:38 +0900

    05_support_vector_machine_Extra.pyを追加する。

---
+++ MachineLearning/handson_ml_master/05_support_vector_machines_Extra.py
@@ -0,0 +1,188 @@
+# common library
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.base import BaseEstimator
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVR
+from sklearn.svm import SVC
+from sklearn.svm import SVR
+from sklearn.linear_model import SGDClassifier
+
+print('------------------------------------------------------------------------------------------------------\n'
+      '                           Extra material                                                             \n'
+      '------------------------------------------------------------------------------------------------------\n')
+print('---< training time >---')
+X, y = make_moons(n_samples=1000, noise=0.4, random_state=42)
+plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "bs")
+plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "g^")
+plt.show()
+
+tol = 0.1
+tols = []
+times = []
+
+for i in range(10):
+    svm_clf = SVC(kernel='poly', gamma=3, C=10, tol=tol, verbose=1)
+    t1 = time.time()
+    svm_clf.fit(X, y)
+    t2 = time.time()
+    times.append(t2 - t1)
+    tols.append(tol)
+    print(i, tol, t2 - t1)
+    tol /= 10
+
+plt.semilogx(tols, times)
+plt.show()
+
+print('------------------------------------------------------------------------------------------------------\n'
+      '          Linear SVM classifier implementation using Batch Gradient Descent                           \n'
+      '------------------------------------------------------------------------------------------------------\n')
+# Get iris from the dataset.
+iris = datasets.load_iris()
+
+# Training set
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = (iris["target"] == 2).astype(np.float64).reshape(-1, 1)  # Iris-Virginica
+
+
+class MyLinearSVC(BaseEstimator):
+    def __init__(self, C=1, eta0=1, eta_d=10000, n_epochs=1000, random_state=None):
+        self.C = C
+        self.eta0 = eta0
+        self.n_epochs = n_epochs
+        self.random_state = random_state
+        self.eta_d = eta_d
+
+    def eta(self, epoch):
+        return self.eta0 / (epoch + self.eta_d)
+
+    def fit(self, X, y):
+        # Random initialization
+        if self.random_state:
+            np.random.seed(self.random_state)
+        w = np.random.randn(X.shape[1], 1)  # n feature weights
+        b = 0
+
+        m = len(X)
+        t = y * 2 - 1  # -1 if t==0, +1 if t==1
+        X_t = X * t
+        self.Js = []
+
+        # Training
+        for epoch in range(self.n_epochs):
+            support_vectors_idx = (X_t.dot(w) + t * b < 1).ravel()
+            X_t_sv = X_t[support_vectors_idx]
+            t_sv = t[support_vectors_idx]
+
+            J = 1 / 2 * np.sum(w * w) + self.C * (np.sum(1 - X_t_sv.dot(w)) - b * np.sum(t_sv))
+            self.Js.append(J)
+
+            w_gradient_vector = w - self.C * np.sum(X_t_sv, axis=0).reshape(-1, 1)
+            b_derivative = -C * np.sum(t_sv)
+
+            w = w - self.eta(epoch) * w_gradient_vector
+            b = b - self.eta(epoch) * b_derivative
+
+        self.intercept_ = np.array([b])
+        self.coef_ = np.array([w])
+        support_vectors_idx = (X_t.dot(w) + t * b < 1).ravel()
+        self.support_vectors_ = X[support_vectors_idx]
+        return self
+
+    def decision_function(self, X):
+        return X.dot(self.coef_[0]) + self.intercept_[0]
+
+    def predict(self, X):
+        return (self.decision_function(X) >= 0).astype(np.float64)
+
+
+C = 2
+svm_clf = MyLinearSVC(C=C, eta0=10, eta_d=1000, n_epochs=60000, random_state=2)
+svm_clf.fit(X, y)
+svm_clf_predict = svm_clf.predict(np.array([[5, 2], [4, 1]]))
+print('svm_clf_predict = \n{0}'.format(svm_clf_predict))
+print()
+
+plt.plot(range(svm_clf.n_epochs), svm_clf.Js)
+plt.axis([0, svm_clf.n_epochs, 0, 100])
+plt.show()
+print()
+
+print('svm_clf.intercept_ = {0}\n svm_clf.coef_ = \n{1}'.format(svm_clf.intercept_, svm_clf.coef_))
+print()
+
+svm_clf2 = SVC(kernel="linear", C=C)
+svm_clf2.fit(X, y.ravel())
+print('svm_clf2.intercept_ = {0}\n svm_clf2.coef_ = \n{1}'.format(svm_clf2.intercept_, svm_clf2.coef_))
+print()
+
+
+def plot_svc_decision_boundary(svm_clf, xmin, xmax):
+    w = svm_clf.coef_[0]
+    b = svm_clf.intercept_[0]
+
+    # At the decision boundary, w0*x0 + w1*x1 + b = 0
+    # => x1 = -w0/w1 * x0 - b/w1
+    x0 = np.linspace(xmin, xmax, 200)
+    decision_boundary = -w[0] / w[1] * x0 - b / w[1]
+
+    margin = 1 / w[1]
+    gutter_up = decision_boundary + margin
+    gutter_down = decision_boundary - margin
+
+    svs = svm_clf.support_vectors_
+    plt.scatter(svs[:, 0], svs[:, 1], s=180, facecolors='#FFAAAA')
+    plt.plot(x0, decision_boundary, "k-", linewidth=2)
+    plt.plot(x0, gutter_up, "k--", linewidth=2)
+    plt.plot(x0, gutter_down, "k--", linewidth=2)
+
+
+yr = y.ravel()
+plt.figure(figsize=(12, 4.0))
+plt.subplot(121)
+plt.plot(X[:, 0][yr == 1], X[:, 1][yr == 1], "g^", label="Iris-Virginica")
+plt.plot(X[:, 0][yr == 0], X[:, 1][yr == 0], "bs", label="Not Iris-Virginica")
+plot_svc_decision_boundary(svm_clf, 4, 6)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.title("MyLinearSVC", fontsize=14)
+plt.axis([4, 6, 0.8, 2.8])
+
+plt.subplot(122)
+plt.plot(X[:, 0][yr == 1], X[:, 1][yr == 1], "g^")
+plt.plot(X[:, 0][yr == 0], X[:, 1][yr == 0], "bs")
+plot_svc_decision_boundary(svm_clf2, 4, 6)
+plt.xlabel("Petal length", fontsize=14)
+plt.title("SVC", fontsize=14)
+plt.axis([4, 6, 0.8, 2.8])
+plt.show()
+
+sgd_clf = SGDClassifier(loss="hinge", alpha=0.017, max_iter=50, tol=-np.infty, random_state=42)
+sgd_clf.fit(X, y.ravel())
+
+m = len(X)
+t = y * 2 - 1  # -1 if t==0, +1 if t==1
+X_b = np.c_[np.ones((m, 1)), X]  # Add bias input x0=1
+X_b_t = X_b * t
+sgd_theta = np.r_[sgd_clf.intercept_[0], sgd_clf.coef_[0]]
+print('sgd_theta = {0}'.format(sgd_theta))
+support_vectors_idx = (X_b_t.dot(sgd_theta) < 1).ravel()
+sgd_clf.support_vectors_ = X[support_vectors_idx]
+sgd_clf.C = C
+
+plt.figure(figsize=(5.5, 3.2))
+plt.plot(X[:, 0][yr == 1], X[:, 1][yr == 1], "g^")
+plt.plot(X[:, 0][yr == 0], X[:, 1][yr == 0], "bs")
+plot_svc_decision_boundary(sgd_clf, 4, 6)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.title("SGDClassifier", fontsize=14)
+plt.axis([4, 6, 0.8, 2.8])
+plt.show()
