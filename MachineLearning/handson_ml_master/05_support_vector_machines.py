59d6bf25e62846dfccf669c8d8c58b362e2fb214
munezou <kazumikm0119@pi5.fiberbit.net>
2019-11-04 11:50:17 +0900

    05_support_vector_machine.pyに、source codeを追加する。

--- MachineLearning/handson_ml_master/05_support_vector_machines.py
+++ MachineLearning/handson_ml_master/05_support_vector_machines.py
@@ -13,6 +13,7 @@ from sklearn.datasets import make_moons
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.svm import LinearSVR
 from sklearn.svm import SVR
+from mpl_toolkits.mplot3d import Axes3D
 
 
 # to make this notebook's output stable across runs
@@ -600,8 +601,121 @@ plt.title(r"$degree={}, C={}, \epsilon = {}$".format(svm_poly_reg2.degree, svm_p
 save_fig("svm_with_polynomial_kernel_plot")
 plt.show()
 
+'''
+------------------------------------------------------------------------------------------------------------------------
+                                   5.4 Under the hood
+------------------------------------------------------------------------------------------------------------------------
+'''
+
 print('-----------------------------------------------------------------------------------------------------------------\n'
-      '                                5.4 Under the hood                                                               \n'
+      '                                5.4.1 Decision functions and predictions                                         \n'
       '-----------------------------------------------------------------------------------------------------------------\n')
 iris = datasets.load_iris()
+
+print(iris.DESCR)
+
+# petal length, petal width
+X = iris["data"][:, (2, 3)]
+
+# Iris-Virginica
+y = (iris["target"] == 2).astype(np.float)
+
+def plot_3D_decision_function(ax, w, b, x1_lim=[4, 6], x2_lim=[0.8, 2.8]):
+    x1_in_bounds = (X[:, 0] > x1_lim[0]) & (X[:, 0] < x1_lim[1])
+    X_crop = X[x1_in_bounds]
+    y_crop = y[x1_in_bounds]
+    x1s = np.linspace(x1_lim[0], x1_lim[1], 20)
+    x2s = np.linspace(x2_lim[0], x2_lim[1], 20)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    xs = np.c_[x1.ravel(), x2.ravel()]
+    df = (xs.dot(w) + b).reshape(x1.shape)
+    m = 1 / np.linalg.norm(w)
+    boundary_x2s = -x1s*(w[0]/w[1])-b/w[1]
+    margin_x2s_1 = -x1s*(w[0]/w[1])-(b-1)/w[1]
+    margin_x2s_2 = -x1s*(w[0]/w[1])-(b+1)/w[1]
+    ax.plot_surface(x1s, x2, np.zeros_like(x1),
+                    color="b", alpha=0.2, cstride=100, rstride=100)
+    ax.plot(x1s, boundary_x2s, 0, "k-", linewidth=2, label=r"$h=0$")
+    ax.plot(x1s, margin_x2s_1, 0, "k--", linewidth=2, label=r"$h=\pm 1$")
+    ax.plot(x1s, margin_x2s_2, 0, "k--", linewidth=2)
+    ax.plot(X_crop[:, 0][y_crop==1], X_crop[:, 1][y_crop==1], 0, "g^")
+    ax.plot_wireframe(x1, x2, df, alpha=0.3, color="k")
+    ax.plot(X_crop[:, 0][y_crop==0], X_crop[:, 1][y_crop==0], 0, "bs")
+    ax.axis(x1_lim + x2_lim)
+    ax.text(4.5, 2.5, 3.8, "Decision function $h$", fontsize=15)
+    ax.set_xlabel(r"Petal length", fontsize=15)
+    ax.set_ylabel(r"Petal width", fontsize=15)
+    ax.set_zlabel(r"$h = \mathbf{w}^T \mathbf{x} + b$", fontsize=18)
+    ax.legend(loc="upper left", fontsize=16)
+
+fig = plt.figure(figsize=(11, 6))
+ax1 = fig.add_subplot(111, projection='3d')
+plot_3D_decision_function(ax1, w=svm_clf2.coef_[0], b=svm_clf2.intercept_[0])
+
+#save_fig("iris_3D_plot")
+plt.show()
+
+print('-----------------------------------------------------------------------------------------------------------------\n'
+      '                                5.4.2 Training goals                                                             \n'
+      '-----------------------------------------------------------------------------------------------------------------\n')
+
+print('---< Small weight vector results in a large margin >---')
+
+def plot_2D_decision_function(w, b, ylabel=True, x1_lim=[-3, 3]):
+    x1 = np.linspace(x1_lim[0], x1_lim[1], 200)
+    y = w * x1 + b
+    m = 1 / w
+
+    plt.plot(x1, y)
+    plt.plot(x1_lim, [1, 1], "k:")
+    plt.plot(x1_lim, [-1, -1], "k:")
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+    plt.plot([m, m], [0, 1], "k--")
+    plt.plot([-m, -m], [0, -1], "k--")
+    plt.plot([-m, m], [0, 0], "k-o", linewidth=3)
+    plt.axis(x1_lim + [-2, 2])
+    plt.xlabel(r"$x_1$", fontsize=16)
+    if ylabel:
+        plt.ylabel(r"$w_1 x_1$  ", rotation=0, fontsize=16)
+    plt.title(r"$w_1 = {}$".format(w), fontsize=16)
+
+plt.figure(figsize=(12, 3.2))
+plt.subplot(121)
+plot_2D_decision_function(1, 0)
+plt.subplot(122)
+plot_2D_decision_function(0.5, 0, ylabel=False)
+save_fig("small_w_large_margin_plot")
+plt.show()
+print()
+
+X = iris["data"][:, (2, 3)] # petal length, petal width
+y = (iris["target"] == 2).astype(np.float64) # Iris-Virginica
+
+svm_clf = SVC(kernel="linear", C=1)
+svm_clf.fit(X, y)
+svm_clf_predit = svm_clf.predict([[5.3, 1.3]])
+print('svm_clf_predit = {0}'.format(svm_clf_predit))
+print()
+
+print('-----------------------------------------------------------------------------------------------------------------\n'
+      '                                5.4.6 on-line SVM                                                                \n'
+      '-----------------------------------------------------------------------------------------------------------------\n')
+print('---< hingle loss >---')
+
+t = np.linspace(-2, 4, 200)
+h = np.where(1 - t < 0, 0, 1 - t)  # max(0, 1-t)
+
+plt.figure(figsize=(5,2.8))
+plt.plot(t, h, "b-", linewidth=2, label="$max(0, 1 - t)$")
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+plt.axvline(x=0, color='k')
+plt.yticks(np.arange(-1, 2.5, 1))
+plt.xlabel("$t$", fontsize=16)
+plt.axis([-2, 4, -1, 2.5])
+plt.legend(loc="upper right", fontsize=16)
+save_fig("hinge_plot")
+plt.show()
+print()
 
