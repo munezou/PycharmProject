98f62565f7045ab593dc10cb37b06649d316fc36
munezou <kazumikm0119@pi5.fiberbit.net>
2019-11-09 22:25:26 +0900

    削除

--- normal/FluentPython-code-master/07-closure-deco/clockdeco_param_demo1.py
+++
@@ -1,9 +0,0 @@
-import time
-from normal.PythonDataModel.ch07_closure_deco.clockdeco_param import clock
-
-@clock('{name}: {elapsed}s')
-def snooze(seconds):
-    time.sleep(seconds)
-
-for i in range(3):
-    snooze(.123)
--- normal/FluentPython-code-master/attic/strings-bytes/identifier_norm.py
+++
@@ -1,7 +0,0 @@
-
-cafe = 1
-cafe = 2
-names = {(name, tuple(name)):value
-         for name, value in globals().items()
-         if not name.startswith('__')}
-print(names)
