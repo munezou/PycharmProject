4d165bdbfc7c2f59e288b4f5430821fc3777b987
munezou <kazumikm0119@33.catv-yokohama.ne.jp>
2024-09-01 03:09:06 +0900

    tensorflow216

---
+++ alternative-isg-analyze2_tf216/gender.py
@@ -0,0 +1,6 @@
+from enum import IntEnum
+
+
+class Gender(IntEnum):
+    Male = 0
+    Female = 1
---
+++ alternative-isg-analyze2_tf216/result.py
@@ -0,0 +1,59 @@
+from xml.dom import minidom
+from datetime import datetime, timedelta
+
+
+class ScoringResult:
+    type_str: str
+    start_mills: int
+
+    def __init__(self, type_str: str, start_mills: int):
+        self.type_str = type_str
+        self.start_mills = start_mills
+
+    def to_list(self):
+        return [self.type_str, self.start_mills]
+
+    @staticmethod
+    def from_rml(source_file: str) -> list:
+        xml = minidom.parse(source_file)
+        stages = xml.getElementsByTagName('Stage')
+        start_time = xml.getElementsByTagName("RecordingStart")[0]
+        during_time = xml.getElementsByTagName("Duration")[0]
+        start_time = start_time.firstChild.data
+        start_time = start_time.replace('T', ' ')
+        start_time_object = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+        during_time = during_time.firstChild.data
+        during_time_int = int(during_time)
+        finish_time_object = start_time_object + during_time_int * timedelta(seconds=1)
+        past_delta_time: int = 0
+        past_t: int = 0
+        result = []
+        for s in stages:
+            t = s.attributes['Type'].value
+            start = s.attributes['Start'].value
+            delta_time = int(start)
+            if delta_time != 0:
+                during_epoc = int((delta_time - past_delta_time) / 30)
+
+                for i in range(during_epoc):
+                    if i != 0:
+                        input_time = past_delta_time + i * 30
+                        measuring_time = start_time_object + input_time * timedelta(seconds=1)
+                        result.append(ScoringResult(str(measuring_time), past_t))
+
+            measuring_time = start_time_object + delta_time * timedelta(seconds=1)
+            result.append(ScoringResult(str(measuring_time), t))
+
+            past_measuring_time = measuring_time
+            past_t = t
+            past_delta_time = delta_time
+
+        if finish_time_object > past_measuring_time:
+            num_epoc_fill = int((during_time_int - past_delta_time) / 30)
+            for j in range(num_epoc_fill + 1):
+                if j != 0:
+                    input_time = past_delta_time + j * 30
+                    measuring_time = start_time_object + input_time * timedelta(seconds=1)
+                    result.append(ScoringResult(str(measuring_time), past_t))
+
+        return result
